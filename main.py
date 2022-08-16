7b7c36327aea6503aa30e486cdac8b0a11cb0d3e
lucasxxkz <111291719+lucasgabriel2006br@users.noreply.github.com>
2022-08-16 16:04:53 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+print("*********************************************")
+print("calculadora de formula de física e matematica")
+print("*********************************************")
+escolha = int(input("qual calculadora usar?"))\n  [1] matematica [2] física\n"))
+
+if escolha == 1:
+print("Essa é a minha calculadorade pitagoras!\n ")
+print("a formula de pitagoras é A² = B²+ C²\n ")
+co = float(input("qual é o valor do cateto oposto?\n"))
+
+print("Essa é minha calculadora de pitagoras!")
+print("a formula de pitagoras é A²= B²= C²)
